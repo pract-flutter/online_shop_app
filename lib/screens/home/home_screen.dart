5361e352950ff3e7c9f23dd082a3b7203e41fa6a
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-07-13 11:46:28 +0700

    Decorate header

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_shop_app/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset('assets/icons/back.svg'),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/search.svg',
+            // By deafult, our icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/cart.svg',
+            // By deafult, our icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: kDefaultPadding / 2,
+        ),
+      ],
+    );
+  }
+}
