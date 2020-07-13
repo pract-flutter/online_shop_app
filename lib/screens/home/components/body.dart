b5f3e06535c6e2666aa534b5b0a3928972c386c4
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-07-13 12:45:44 +0700

    Add navigator to DetailScreen

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:online_shop_app/constants.dart';
 import 'package:online_shop_app/models/Product.dart';
+import 'package:online_shop_app/screens/details/detail_screen.dart';
 import 'package:online_shop_app/screens/home/components/categories.dart';
 import 'package:online_shop_app/screens/home/components/item_card.dart';
 
@@ -25,15 +26,23 @@ class Body extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
             child: GridView.builder(
-                itemCount: products.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: kDefaultPadding,
-                  crossAxisSpacing: kDefaultPadding,
-                  childAspectRatio: 0.75,
-                ),
-                itemBuilder: (context, index) =>
-                    ItemCard(product: products[index])),
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: kDefaultPadding,
+                crossAxisSpacing: kDefaultPadding,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) => ItemCard(
+                product: products[index],
+                press: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailsScreen(
+                              product: products[index],
+                            ))),
+              ),
+            ),
           ),
         )
       ],
